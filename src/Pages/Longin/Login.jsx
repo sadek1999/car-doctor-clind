e7453db3a884d10e44613ef67d15885e120c55fc
Sadek Ali <sadekali1530@gmail.com>
2023-10-31 21:16:50 +0600

    add the code

--- src/Pages/Longin/Login.jsx
+++ src/Pages/Longin/Login.jsx
@@ -1,15 +1,15 @@
 
-
+  
 const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <div className="text-center ">
-                    
-                  
+                <div className="text-center lg:w-1/2">
+                   <img src='https://img.freepik.com/free-vector/sign-concept-illustration_114360-125.jpg?w=740&t=st=1698765111~exp=1698765711~hmac=ec2a65f690ade588966e9469140311678d72ce91cca4eb54c880b1957aff7ddf' alt="" />
+
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <h1 className="text-5xl font-bold text-center">Login now!</h1>
+                <div className="card flex-shrink-0 w-full max-w-sm lg:w-1/2 shadow-2xl bg-base-100">
+                    <h1 className="text-5xl font-bold text-center">Login now!</h1>
                     <form className="card-body">
                         <div className="form-control">
                             <label className="label">
