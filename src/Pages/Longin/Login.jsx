9a46345c535fd4930c2b70e2fdd97f2407d3cecd
Sadek Ali <sadekali1530@gmail.com>
2023-10-31 21:22:27 +0600

    add the code

--- src/Pages/Longin/Login.jsx
+++ src/Pages/Longin/Login.jsx
@@ -1,6 +1,11 @@
 
   
 const Login = () => {
+
+    const handlLogin=e=>{
+        e.preventDefault()
+
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -10,24 +15,25 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm lg:w-1/2 shadow-2xl bg-base-100">
                     <h1 className="text-5xl font-bold text-center">Login now!</h1>
-                    <form className="card-body">
+                    <form className="card-body" onSubmit={handlLogin}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                           
+                            <input type="submit" className="btn btn-primary" value="Login" />
                         </div>
                     </form>
                 </div>
